760b623163194cc0e81300981a47249e59a5f29b
Matheus Salaroli <mat.s@terra.com.br>
2022-07-12 10:21:24 -0300

    correções do docker-compose

--- raspberry/sb-docker/build.sh
+++ raspberry/sb-docker/build.sh
@@ -5,7 +5,7 @@ docker-compose build
 docker-compose up -d
 docker-compose down
 
-cp ./mosquitto-conf/mosquitto.conf /var/lib/docker/volumes/sb-docker_mosquitto-conf/_data/mosquitto.conf
+cp ./mosquitto-conf/mosquitto.conf /var/lib/docker/volumes/sb-docker_mosquitto-config/_data/mosquitto.conf
 
 rm /var/lib/docker/volumes/sb-docker_node-red-data/_data/settings.js
 cp ./node-red-data/settings.js /var/lib/docker/volumes/sb-docker_node-red-data/_data/settings.js
@@ -13,7 +13,7 @@ cp ./node-red-data/settings.js /var/lib/docker/volumes/sb-docker_node-red-data/_
 chmod 777 /var/lib/docker/volumes/sb-docker_node-red-data/_data/settings.js
 chown 1000:1000 /var/lib/docker/volumes/sb-docker_node-red-data/_data/settings.js
 
-chmod 777 /var/lib/docker/volumes/sb-docker_mosquitto-conf/_data/mosquitto.conf
-chown 1000:1000 /var/lib/docker/volumes/sb-docker_mosquitto-conf/_data/mosquitto.conf
+chmod 777 /var/lib/docker/volumes/sb-docker_mosquitto-config/_data/mosquitto.conf
+chown 1000:1000 /var/lib/docker/volumes/sb-docker_mosquitto-config/_data/mosquitto.conf
 
 docker-compose up -d
