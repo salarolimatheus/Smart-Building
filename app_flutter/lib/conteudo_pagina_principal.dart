6b29a11d1646efba101d2bd95fdacaca27c92136
Pedro Pincinato Gut <pedropincinatogut@gmail.com>
2022-07-26 13:32:12 -0300

    Botão de power incluido e tela parcialmente criada

--- app_flutter/lib/conteudo_pagina_principal.dart
+++ app_flutter/lib/conteudo_pagina_principal.dart
@@ -6,7 +6,52 @@ class conteudoPaginaPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: botao(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Tomada 1",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(20)),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Tomada 2",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+              Padding(padding: EdgeInsets.all(10)),
+              botao(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -20,19 +65,21 @@ class _botaoState extends State<botao> {
   Color cor_botao = Colors.red;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ElevatedButton.icon(
-        onPressed: () {
-          if (cor_botao == Colors.red) {
-            cor_botao = Colors.green;
-          } else {
-            cor_botao = Colors.red;
-          }
-          setState(() {});
-        },
-        icon: Icon(Icons.power_settings_new, size: 25),
-        label: Text("Tomada 1"),
-        style: ElevatedButton.styleFrom(primary: cor_botao),
+    return TextButton(
+      onPressed: () {
+        if (cor_botao == Colors.red) {
+          cor_botao = Colors.green;
+        } else {
+          cor_botao = Colors.red;
+        }
+        setState(() {});
+      },
+      child: Icon(Icons.power_settings_new, size: 50),
+      style: TextButton.styleFrom(
+        primary: Colors.white,
+        backgroundColor: cor_botao,
+        fixedSize: Size(150, 100),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
       ),
     );
   }
